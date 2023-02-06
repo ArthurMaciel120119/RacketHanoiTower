451d5b616822c1d192134b398793e80cde656525
Arthurzin da Exatas <84238286+ArthurMaciel120119@users.noreply.github.com>
2023-02-06 08:41:46 -0300

    Update README.rkt

--- CodigoJogo/README.rkt
+++ CodigoJogo/README.rkt
@@ -1,1 +1,17 @@
+;REGRAS
+
+1.Deslocar um disco de cada vez, o qual deverá ser o do topo de uma das três torres;
+2.Cada disco nunca poderá ser colocado sobre outro de temanho menor.
+3.Partida para 1 jogador.
+
+;OBJETIVO
+O objetivo do jogo consiste em deslocar todos os discos da torre onde se encontram para uma torre diferente.
+
+;JOGABILIDADE
+De acordo com as regras do jogo, para que o maior disco possa ser colocado
+no eixo da direita, é obrigatório que os discos menores estejam empilhados 
+na torre do meio tendo o maior disco sozinho no eixo à esquerda. A tarefa
+de passar os discos menores da torre da esquerda para a torre do meio é
+equivalente (em termos de números de movimentos necessários) à de passar os
+discos da torre do meio para a torre da direita.
 
